bb5f957e87d85add9e1af2457b7ff655ac7c140f
himanshhhhuv <himanshhhhuv@gmail.com>
2024-02-01 13:27:56 +0530

    Uploaded Hero page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:
+      {
+        fontFamily: {
+          Poppins: ['Poppins', 'sans-serif'], // Use a meaningful name for the font family
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
